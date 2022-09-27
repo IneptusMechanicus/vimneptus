16d6e1d04c5b172dac3e588d24a69b27cb90f790
Ivo <ivo@pop-os.localdomain>
2022-09-27 16:43:05 +0300

    Changing packer config to work with older git version

--- lua/setup.lua
+++ lua/setup.lua
@@ -22,7 +22,24 @@ end
 --------------------------
 
 require('packer').startup(function(use)
-	use {'wbthomason/packer.nvim'}
+	use {'wbthomason/packer.nvim',
+		git = {
+			cmd = 'git', -- The base command for git operations
+			subcommands = { -- Format strings for git subcommands
+				update = 'pull --ff-only --progress',
+				install = 'clone --depth %i --no-single-branch --progress',
+				fetch = 'fetch --depth 999999 --progress',
+				checkout = 'checkout %s --',
+				update_branch = 'merge --ff-only @{u}',
+				current_branch = 'branch --show-current',
+				diff = 'log --color=never --pretty=format:FMT --no-show-signature HEAD@{1}...HEAD',
+				diff_fmt = '%%h %%s (%%cr)',
+				get_rev = 'rev-parse --short HEAD',
+				get_msg = 'log --color=never --pretty=format:FMT --no-show-signature HEAD -n 1',
+				submodules = 'submodule update --init --recursive --progress'
+			},
+		},
+	}
 	use {'nvim-lualine/lualine.nvim'}
 	use {'famiu/bufdelete.nvim'}
 	use {'nvim-lua/popup.nvim'}

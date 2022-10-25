e3301223ee693aa4567eca0308afe0057a6b3913
Ivo <ivo@pop-os.localdomain>
2022-10-25 10:04:07 +0300

    Minor Tweeks to options and keybindings

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -1,14 +1,11 @@
 local binding = vim.keymap
 local g = vim.g
---------------------------
---     Keybindings      --
---------------------------
 g.mapleader = ' '
 
 -- Main
+binding.set('n', '<leader>q','<cmd>Bdelete<cr>')
 binding.set('n', '<leader>w', '<cmd>write<cr>')
 binding.set('n', '<leader>a', ':keepjumps normal! ggVG<cr>')
-binding.set('n', '<leader>q','<cmd>Bdelete<cr>')
 binding.set('n', '<C-q>', '<cmd>qa<cr>')
 
 -- Clipboard hotkeys
@@ -25,17 +22,12 @@ binding.set('n', '<S-Right>', '<cmd>BufferLineCycleNext<cr>')
 binding.set('n', '<S-Left>', '<cmd>BufferLineCyclePrev<cr>')
 
 -- Comments --
-
 binding.set('n', '<C-/>', '<cmd>CommentToggle<cr>')
 binding.set({'v', 'x'}, '<C-/>', '<cmd>\'<,\'>CommentToggle<cr>')
 
 -- Moving lines --
-
 binding.set('n', '<C-Up>', 'ddkkp')
 binding.set('n', '<C-Down>', 'ddp')
 
-binding.set('x', '<C-Up>', 'dkkp')
-binding.set('x', '<C-Down>', 'dp')
 -- Trouble Toggle --
-
 binding.set('n', '<leader>t', '<cmd>TroubleToggle<cr>')
--- lua/options.lua
+++ lua/options.lua
@@ -1,9 +1,6 @@
---------------------------
--- Base Editor Options  --
---------------------------
+local vim = vim
 
 -- .1 General --
-
 vim.opt.number = true
 vim.opt.numberwidth = 5
 vim.opt.mouse = 'a'
@@ -12,17 +9,16 @@ vim.opt.lazyredraw = true
 vim.opt.wrap = false
 
 -- .2 Search --
-
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.hlsearch = true
 
 -- .3 Tabs, Spaces, Newlines --
-
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = false
 vim.opt.autoindent = true
 
+-- 4. Special characters
 vim.opt.fillchars='eob: '
 vim.opt.listchars='tab:| ,trail:*,eol:¬'

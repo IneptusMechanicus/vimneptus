37d87f7770358ffc30742e53040896bb24db96d2
Ivo <ivo@pop-os.localdomain>
2023-01-08 18:19:34 +0200

    Extracted colorscheme from config

--- init.lua
+++ init.lua
@@ -3,7 +3,7 @@ require('keybindings')
 require('commands')
 require('setup')
 
-local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/lualine.nvim'
-if vim.fn.empty(vim.fn.glob(install_path)) <= 0 then
-	vim.cmd('colorscheme mechanicus')
-end
+-- local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/lualine.nvim'
+-- if vim.fn.empty(vim.fn.glob(install_path)) <= 0 then
+	-- vim.cmd('colorscheme mechanicus')
+-- end

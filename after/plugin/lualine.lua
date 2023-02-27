f30212eb7d6444da49bab54de80c77c203574654
Ivo <ivo@pop-os.localdomain>
2023-02-27 10:43:22 +0200

    Tweaking Lualine

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -8,5 +8,16 @@ lualine.setup({
     component_separators = '|',
     section_separators = ' ',
     disabled_filetypes = {'packer'}
-  }
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff'},
+    lualine_c = {{
+      'filename',
+      path = 1
+    }},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
 })
